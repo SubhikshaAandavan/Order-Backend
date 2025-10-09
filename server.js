2863b538633fa5cf4761165ee3f6c70e40c1d07d
Subhiksha A <subhiaandav@gmail.com>
2025-10-09 10:39:04 +0530

    f1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://order-front-end-chi.vercel.app/"],
+    origin: ["https://order-front-end-chi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
